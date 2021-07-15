adb6a9c89c875652797c53c7d5ce0aef74d4a0c4
Edgar779 <cherqezyancherqezyan97@gmail.com>
2021-07-15 12:33:34 +0400

    bugFix

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from '../admin';
-import { AuthNModule } from '../authN';
-import { AuthZModule } from '../authZ';
+import { AuthNModule } from 'src/authN';
+import { AuthZModule } from 'src/authZ';
 import { MailerModule } from '../mailer';
 import { AppController } from './app.controller';
 import { DatabaseConnection } from './app.database';
 import { AppService } from './app.service';
+import { AddressModule } from '../address';
 @Module({
-  imports: [AuthNModule, AuthZModule],
+  imports: [AuthNModule, AuthZModule, AddressModule, AdminModule],
   controllers: [AppController],
   providers: [AppService, DatabaseConnection],
 })
