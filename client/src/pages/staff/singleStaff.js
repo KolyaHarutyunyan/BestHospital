f2aa73e15ab1f18fd63a4d12a6b4f69e061f2dfb
Harutyun <harutyung@eachbase.com>
2021-08-04 18:01:33 +0400

    Added singleStaff

--- client/src/pages/staff/singleStaff.js
+++ client/src/pages/staff/singleStaff.js
@@ -3,26 +3,6 @@ import {StaffItem} from "@eachbase/fragments/staff";
 
 export const SingleStaff = ({general}) => {
 
-    let raw = {
-        name: 'aaaa',
-        age: 25,
-        max: 2,
-        min: 16,
-        cds: 0,
-        lastName: 'nbbbb'
-    }
-
-    const allowed = ['age', 'cds'];
-
-    const filtered = Object.keys(raw)
-        .filter(key => allowed.includes(key))
-        .reduce((obj, key) => {
-            obj[key] = raw[key];
-            return obj;
-        }, {});
-
-    console.log(filtered,'filtered')
-
     return (
         <>
             <StaffItem />
