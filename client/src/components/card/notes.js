c03bc606130e0807064f0cd781b5987a9cc29894
Harutyun <harutyung@eachbase.com>
2021-09-13 09:30:45 +0400

    fixed conflict

--- client/src/components/card/notes.js
+++ client/src/components/card/notes.js
@@ -188,30 +188,25 @@ export const Notes = ({
                         <SimpleModal
                             openDefault={open}
                             handleOpenClose={handleOpenClose}
-// <<<<<<< stafSytemChanegs
-                            content={<AddNotes closeModal={closeModal} model={model} noteModalTypeInfo={noteModalInfoEdit}
-                                               handleClose={handleOpenClose}/>}
+                            content={
+                                <AddNotes
+                                    closeModal={closeModal}
+                                    model={model}
+                                    noteModalTypeInfo={noteModalInfoEdit}
+                                    handleClose={handleOpenClose}/>
+                            }
                         />
                         <SimpleModal
                             openDefault={openDelModal}
                             handleOpenClose={handleOpenCloseDel}
-                            content={<DeleteElement loader={loader} text='some information'
-                                                    info={deletedData?.deletedName}
-                                                    handleDel={handleDelete} handleClose={handleOpenCloseDel}/>}
-// =======
-//                             content={<AddNotes model={model}
-//                                                noteModalTypeInfo={noteModalInfoEdit}
-//                                                handleClose={handleOpenClose}/> }
-//                             />
-//                         <SimpleModal
-//                             openDefault={openDelModal}
-//                             handleOpenClose={handleOpenCloseDel}
-//                             content={<DeleteElement text='some information'
-//                                                     info={deletedData?.deletedName}
-//                                                     handleDel={handleDelete}
-//                                                     handleClose={handleOpenCloseDel}/>}
-// >>>>>>> develop
-//                         />
+                            content={
+                                <DeleteElement
+                                    loader={loader}
+                                    text='some information'
+                                    info={deletedData?.deletedName}
+                                    handleDel={handleDelete}
+                                    handleClose={handleOpenCloseDel}/>}
+                            />
                     </>
                 }
             </TableContainer>
