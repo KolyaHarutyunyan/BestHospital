77cc75000e819598682de45ee9b4f62292949632
Edgar779 <cherqezyancherqezyan97@gmail.com>
2021-10-27 10:31:04 +0400

    feature/Mileage/Plce

---
+++ api/src/mileage/interceptor/mileage.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ISanitize } from '../../util';
+import { IMileage } from '../interface/mileage.interface';
+import { MileageDTO } from '../dto';
+
+@Injectable()
+export class MileageSanitizer implements ISanitize {
+    constructor() { }
+
+    sanitize(mileage: IMileage): MileageDTO {
+        const mileageDTO: MileageDTO = {
+            _id: mileage.id,
+            compensation: mileage.compensation,
+            startDate: mileage.startDate,
+            endDate: mileage.endDate
+        };
+        return mileageDTO;
+    }
+
+    sanitizeMany(mileages: IMileage[]): MileageDTO[] {
+        const mileageDTOs: MileageDTO[] = [];
+        for (let i = 0; i < mileages.length; i++) {
+            mileageDTOs.push(this.sanitize(mileages[i]));
+        }
+        return mileageDTOs;
+    }
+}
