21f65c720075d547fd532bdc55b77cc52e9dd5ef
KolyaHarutyunyan <kolyah2809@gmail.com>
2022-07-12 02:54:11 +0400

    feature/ help-center-page header-content, font-family

---
+++ lp/src/pages/helpCenter/fragments/constants.js
@@ -0,0 +1,2 @@
+export const helpCenterHeaderContentTitle = "Advice and answers from the Wellness team."
+export const helpCenterHeaderContentSubtitle = "We would be glad to help you and discuss anything."
--- lp/src/pages/helpCenter/fragments/helpCenterFragment.js
+++ lp/src/pages/helpCenter/fragments/helpCenterFragment.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { Card } from "components";
+import { Button, Card } from "components";
+import { helpCenterHeaderContentSubtitle, helpCenterHeaderContentTitle } from "./constants";
 
 export const HelpCenterFragment = () => {
    return (
       <div className="help-center-fragment">
-         <Card cardBackgroundColor={""}>help center fragment</Card>
+         <Card cardClassName={"help-center-page-cover"}>
+            <div className="help-center-header">
+               <div className="header-content">
+                  <h1 className="content-title">{helpCenterHeaderContentTitle}</h1>
+                  <h6 className="content-subtitle">{helpCenterHeaderContentSubtitle}</h6>
+                  <Button
+                     buttonType={"button"}
+                     buttonClassName={"contact-support-button"}
+                     onClickButton={() => {}}
+                  >
+                     Contact Support
+                  </Button>
+               </div>
+            </div>
+         </Card>
+         <Card cardBackgroundColor={""}>help center main</Card>
       </div>
    );
 };
